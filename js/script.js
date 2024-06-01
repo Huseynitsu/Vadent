db85f502a26f2c8eba29c97575573c66b89563b1
Huseynitsu <huseynquliyev395@gmail.com>
2024-06-01 11:29:55 +0400

    changes

--- js/script.js
+++ js/script.js
@@ -30,39 +30,5 @@ $(document).ready(function () {
         centeredSlides: true,
     });
 
-    const { ScrollObserver, valueAtPercentage } = aat
-
-    const $cardsContainer = $('.cards');
-    const $cards = $('.card');
-    $cardsContainer.css('--cards-count', $cards.length);
-    $cardsContainer.css('--card-height', `${$cards.first().height()}px`);
-
-    $cards.each(function (index) {
-        const $card = $(this);
-        const offsetTop = 20 + index * 20;
-        if (index === $cards.length - 1) {
-            return false
-        }
-        const toScale = 1 - ($cards.length - 1 - index) * 0.1;
-        const $nextCard = $cards.eq(index + 1);
-        const $cardInner = $card.find('.card__inner');
-
-        ScrollObserver.Element($nextCard[0], {
-            offsetTop,
-            offsetBottom: $(window).height() - $card.height()
-        }).onScroll(({ percentageY }) => {
-            $cardInner.css('scale', valueAtPercentage({
-                from: 1,
-                to: toScale,
-                percentage: percentageY
-            }));
-            $cardInner.css('filter', `brightness(${valueAtPercentage({
-                from: 1,
-                to: 0.6,
-                percentage: percentageY
-            })})`);
-        });
-    });
-
 
 });
